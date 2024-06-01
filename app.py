d4fc5e9c3fd9e1893fff649ac1d4c9bbd5160fb6
Banasmita Jena <155791058+banasmita24@users.noreply.github.com>
2024-06-02 00:13:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -297,7 +297,3 @@ def delete_job(job_id):
     conn.close()
     flash('Job posting deleted successfully!', 'success')
     return redirect(url_for('admin_dashboard'))
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
